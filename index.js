b53930652784097575bde7ee97c59f3158b84420
Rukshan <rukshancoc@gmail.com>
2024-09-28 10:38:11 +0530

    all files changed

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyCompanent.jsx';
+import CreateObject from './Object.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import DigitalClock from './DigitalClock.jsx';
+import OpenAi from './OpenAi.jsx';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <>
+    {/* <MyComponent/> */}
+    {/* <CreateObject/> */}
+    {/* <DigitalClock/> */}
+    <OpenAi/>
+    </>
+   
+  );
+}
+
+export default App;
