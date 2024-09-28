322b79ba8479faeba0e05fa90a251705e68fa5d8
Rukshan <rukshancoc@gmail.com>
2024-09-28 10:48:54 +0530

    f

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyCompanent.jsx';
+import CreateObject from './Object.jsx'
+
+import DigitalClock from './DigitalClock.jsx';
+import OpenAi from './OpenAi.jsx';
+
+function App() {
+  return (
+    <>
+    {/* <MyComponent/> */}
+    {/* <CreateObject/> */}
+    {/* <DigitalClock/> */}
+    <OpenAi/>
+    </>
+   
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './MyCompanent.jsx';
-import CreateObject from './Object.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import DigitalClock from './DigitalClock.jsx';
-import OpenAi from './OpenAi.jsx';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function App() {
-  return (
-    <>
-    {/* <MyComponent/> */}
-    {/* <CreateObject/> */}
-    {/* <DigitalClock/> */}
-    <OpenAi/>
-    </>
-   
-  );
-}
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
